25e1830952e64b006ee66cca55d9aa4bfe8a230c
Felizardo27 <jp.felizardo27@gmail.com>
2024-02-28 23:49:33 -0300

    Fix Responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import Banner from "./components/Banner/index";
 import Form from "./components/Form";
-import Resultado from "./components/Resultado";
 import styles from "./App.module.css";
 import Historico from "./components/Historico";
 import Resultados from "./components/Resultados";
-
 
 function App() {
   return (
     <>
       <Banner />
 
-      <div className={styles.container}>
+      <div className={styles.container_mt}>
         <div className={styles.container_form_result}>
           <Form />
           <Resultados />
         </div>
+        <div className={styles.container_historico}>
+          <Historico left={true} hist={"Dúzias"} />
+          <Historico hist={"Colunas"} />
+        </div>
+      </div>
 
+      <div className={styles.container_desktop}>
+        <Form />
+        <Resultados />
         <div className={styles.container_historico}>
           <Historico left={true} hist={"Dúzias"} />
           <Historico hist={"Colunas"} />
